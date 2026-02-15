fde3c579b7f77b556ccfb904990ba47bb91f9db5
Raciel Cabello <raciel.2006327@gmail.com>
2026-02-15 04:40:07 -0600

    Add files via upload

---
+++ autostart_once.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# Apply wallpaper using wal
+#feh --bg-fill /home/raciel/Personal/Imagenes/h2e7331duh9f1.png
+#feh --bg-fill /home/raciel/Personal/Imagenes/pyk5qrqu9dxc1.png
+feh --bg-fill /home/raciel/Personal/Imagenes/miles.jpg
+
+
